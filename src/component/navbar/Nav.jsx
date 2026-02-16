2cb8357792bd7434a201dc73953095b37ce92f50
vivek <vs2299043@gmail.com>
2026-02-16 14:20:43 +0530

    adding navbar and link taiwindcss

---
+++ src/component/navbar/Nav.jsx
@@ -0,0 +1,20 @@
+function Nav(){
+    return(
+        <>
+        <nav className="flex justify-between items-center px-10 py-5 bg-[#2596be] text-white">
+            {/* name */}
+            <div><h1 className="font-bold text-lg">Welcome</h1></div>
+            {/* list */}
+            <div>
+                <ul className="flex gap-10 font-semibold">
+                    <li className="hover:text-gray-400 hover:underline hover:underline-offset-1 hover:transition-all hover:duration-1000">home</li>
+                    <li className="hover:text-gray-400 hover:underline hover:underline-offset-1 hover:transition-all hover:duration-1000">projects</li>
+                    <li className="hover:text-gray-400 hover:underline hover:underline-offset-1 hover:transition-all hover:duration-1000">contact</li>
+                    <li className="hover:text-gray-400 hover:underline hover:underline-offset-1 hover:transition-all hover:duration-1000">about</li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+export default Nav
