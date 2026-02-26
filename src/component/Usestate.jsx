a2e4ecfaa88493344647eedb1c5cace0698da89e
unknown <vs2299043@gmail.com>
2026-02-26 22:13:55 +0530

    project in usestate

--- src/component/Usestate.jsx
+++ src/component/Usestate.jsx
@@ -2,12 +2,24 @@ import { useState } from 'react';
 import Nav from './navbar/Nav.jsx';
 import Footer from './footer/Footer.jsx'
 const Usestate = () =>{
-	return(
-           <>
+        const [show , setShow] = useState(false);
+               
+               function clickshow(){
+                   setShow(!show)
+               }
+           		
+	return( 
+
+            
+            <>
+               <div className="h-[100vh]">
            		<Nav />
-           		<h1></h1>
+                <h1>toggle-image</h1>
+                <button className="border bg-slate-500 py-1 px-4 rounded" onClick={()=>{clickshow()}}>click</button>
+           		{ show && <img src="./pexels-agrosales-20804701 (1).jpg" />}
            		<Footer />
+               </div>
            </>
 		  )
 }
-export default Usestate
+export default Usestate;
